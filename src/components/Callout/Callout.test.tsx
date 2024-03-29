c28b703fd9cd22b9c80475bf9857c0e7c86c6842
Kasey Labelle <kasey.l.labelle@gmail.com>
2024-03-29 14:33:57 -0400

    remove ghost/inverse from callout

--- src/components/Callout/Callout.test.tsx
+++ src/components/Callout/Callout.test.tsx
@@ -2,13 +2,6 @@ import { render, screen } from '@testing-library/react';
 import Callout from './Callout';
 
 describe('Callout', () => {
-  it('Renders inverse when prop is present', () => {
-    render(<Callout isinverse>inverse callout!</Callout>);
-
-    expect(screen.getByText('inverse callout!')).toBeInTheDocument();
-    expect(screen.getByText('inverse callout!')).toHaveClass('bsds-callout-inverse');
-  });
-
   it('Renders centered when prop is present', () => {
     render(<Callout isCentered>Centered callout!</Callout>);
 
