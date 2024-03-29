711cd281101d5ddb2d1a56415d18b5a71e568e8d
cblva <chrisbuylova@gmail.com>
2024-03-29 10:53:03 -0400

    Add theming to Breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.stories.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -3,7 +3,7 @@
 // decide if we can/want to share dummy data across components
 
 import type { Meta, StoryObj } from '@storybook/react';
-
+import { componentDecorators } from '../../../.storybook/decorators';
 import Breadcrumbs from './Breadcrumbs';
 
 const meta = {
@@ -12,6 +12,7 @@ const meta = {
   parameters: {
     layout: 'centered'
   },
+  decorators: componentDecorators,
   tags: ['autodocs']
 } satisfies Meta<typeof Breadcrumbs>;
 
